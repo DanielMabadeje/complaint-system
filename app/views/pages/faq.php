eec88813861c965cdf19f7719b9a6d068352db5e
DanielMabadeje <mabadejedaniel1@gmail.com>
2022-01-21 20:38:39 +0100

    Added faq page

--- app/views/pages/faq.php
+++ app/views/pages/faq.php
@@ -1,9 +1,48 @@
-<?php require APPROOT . '/views/inc/header.php'; ?>
-<div class="jumbotron jumbotron-fluid">
-    <div class="container">
-        <h1 class="display-3"><?php echo $data['title'] ?></h1>
-        <p class="lead"><?= $data['description']; ?></p>
-    </div>
+<?php require APPROOT . '/views/inc/header.php';?>
+
+<div>
+    <h2 class="display-3">FAQ</h2>
 </div>
 
-<?php require APPROOT . '/views/inc/footer.php'; ?>
+    <div class="card card-body mb-3">
+        <h4 class="card-title">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, nesciunt?
+        </h4>
+        <div class="bg-light p-2 mb-3">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nostrum facere, ipsum voluptatibus accusantium voluptas et dolores, incidunt harum id autem adipisci, aliquam sunt ullam error amet ducimus. Eius veritatis exercitationem iste quidem aspernatur architecto accusamus, consequuntur eos quae voluptatem.</p>
+            
+        </div>
+    </div>
+
+    <div class="card card-body mb-3">
+        <h4 class="card-title">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, nesciunt?
+        </h4>
+        <div class="bg-light p-2 mb-3">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nostrum facere, ipsum voluptatibus accusantium voluptas et dolores, incidunt harum id autem adipisci, aliquam sunt ullam error amet ducimus. Eius veritatis exercitationem iste quidem aspernatur architecto accusamus, consequuntur eos quae voluptatem.</p>
+            
+        </div>
+    </div>
+
+
+    <div class="card card-body mb-3">
+        <h4 class="card-title">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, nesciunt?
+        </h4>
+        <div class="bg-light p-2 mb-3">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nostrum facere, ipsum voluptatibus accusantium voluptas et dolores, incidunt harum id autem adipisci, aliquam sunt ullam error amet ducimus. Eius veritatis exercitationem iste quidem aspernatur architecto accusamus, consequuntur eos quae voluptatem.</p>
+            
+        </div>
+    </div>
+
+    <div class="card card-body mb-3">
+        <h4 class="card-title">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, nesciunt?
+        </h4>
+        <div class="bg-light p-2 mb-3">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nostrum facere, ipsum voluptatibus accusantium voluptas et dolores, incidunt harum id autem adipisci, aliquam sunt ullam error amet ducimus. Eius veritatis exercitationem iste quidem aspernatur architecto accusamus, consequuntur eos quae voluptatem.</p>
+            
+        </div>
+    </div>
+
+<?php require APPROOT . '/views/inc/footer.php';?>
